b173d2c12d57a551274befa3270814248f07243c
Thonia Senna <thonia@poli.ufrj.br>
2019-08-14 07:01:14 -0300

    finish neuralnets-1

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -22,7 +22,16 @@ p = zeros(size(X, 1), 1);
 %
 
 
+a1 = [ones(m, 1) X];
 
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+
+a2 = [ones(m, 1) a2];
+z3 = a2 * Theta2';
+hypo = sigmoid(z3);
+[probability indices] = max(hypo');
+p = indices';
 
 
 
--- machine-learning-ex3/ex3/predictOneVsAll.m
+++ machine-learning-ex3/ex3/predictOneVsAll.m
@@ -28,10 +28,13 @@ X = [ones(m, 1) X];
 %       max element, for more information see 'help max'. If your examples 
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
-%       
-
-
-
+%  
+%[Y,I] = max(X)--- pega o valor máximo em cada coluna de X. Armazena o
+%índice I e o valor máximo será armazenado em Y
+[probabilidade,indice]=max(sigmoid(all_theta*X'));
+p=indice';
+        
+    
 
 
 
